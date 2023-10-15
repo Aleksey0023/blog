86ef71ddc653c344f30a179d0c925a3d918f54b2
Aleksey0023 <alekseyphpjob@gmail.com>
2023-10-15 21:16:11 +0700

    Реализовал функционал страницы "Обо мне"

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -35,7 +35,7 @@
                         <a class="nav-link" href="{{route('review.index')}}">Отзывы</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Обо мне</a>
+                        <a class="nav-link" href="{{route('about.index')}}">Обо мне</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Галерея</a>
