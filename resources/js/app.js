43a70df12b28bb30aa1373b76850b3d069b7f33e
Aleksey0023 <alekseyphpjob@gmail.com>
2023-10-29 21:33:54 +0700

    Удалил папку с плагинами и реализовал сборку проекта через Vite

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,43 @@
-import './bootstrap';
+import $ from 'jquery';
+import 'popper.js';
+import 'bootstrap';
+import AOS from 'aos';
+import select2 from 'select2';
+import 'moment';
+import 'daterangepicker';
+import 'summernote/dist/summernote-lite.min.js';
+import 'overlayscrollbars';
+import bsCustomFileInput from "bs-custom-file-input";
+import 'admin-lte';
+
+$(window).on('load', function () {
+    $(".edica-loader").fadeOut("slow");
+});
+
+$('.edica-header .dropdown').hover(function () {
+    $(this).find('.dropdown-menu').first().stop(true, true).delay(250).slideDown();
+}, function () {
+    $(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp();
+});
+
+AOS.init({
+    duration: 1000
+});
+
+$(document).ready(function () {
+    $('#summernote').summernote({
+        toolbar: [
+            ['style', ['bold', 'italic', 'underline', 'clear']],
+            ['font', ['strikethrough', 'superscript', 'subscript']],
+            ['fontsize', ['fontsize']],
+            ['color', ['color']],
+            ['para', ['ul', 'ol', 'paragraph']],
+            ['height', ['height']]
+        ]
+    });
+
+    bsCustomFileInput.init();
+
+    select2($);
+    $('.select2').select2();
+});
